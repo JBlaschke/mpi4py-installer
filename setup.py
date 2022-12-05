142f97bb4132f6871a14667b9df6247a468b9534
Johannes Blaschke <jpblaschke@lbl.gov>
2022-12-05 14:45:12 -0800

    forgot the __init__.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mpi4py-installer",
-    version="0.1.0",
+    version="0.1.1",
     author="Johannes Blaschke",
     author_email="jblaschke@lbl.gov",
     description="No more copy+paste of magic bash commands while installing mpi4py on HPC systems.",
