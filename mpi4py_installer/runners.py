a7a40edadf769ecc23ff088f0781291e44b0eba3
Johannes Blaschke <jpblaschke@lbl.gov>
2022-12-05 11:54:09 -0800

    more performant communications

--- mpi4py_installer/runners.py
+++ mpi4py_installer/runners.py
@@ -13,6 +13,9 @@ class ShellRunner(AbstractContextManager):
     directly to adjust the environment, or read to get variables.
     """
 
+    # max size of payload size descriptor in bytes
+    _BSC: int = 16
+
     def __init__(self, env=None):
         self.env: dict[str, str]
         if env is None:
@@ -27,9 +30,14 @@ class ShellRunner(AbstractContextManager):
         write_env_pycode = ";".join([
             "import os",
             "import json",
+            # capture env as json
             "env_json = json.dumps(dict(os.environ)).encode()",
-            f"os.write({self._fd_write}, env_json)",
-            f"os.write({self._fd_write}, \"\\0\".encode())"
+            # send payload size ahead of message
+            "plen = len(env_json)",
+            f"plen = plen.to_bytes({self._BSC}, \"big\", signed=False)",
+            f"os.write({self._fd_write}, plen)",
+            # send env json payload
+            f"os.write({self._fd_write}, env_json)"
         ])
         self._env_snapshot = "\n".join([
             "__ShellRunner_exit_code_trap=$?",
@@ -50,13 +58,24 @@ class ShellRunner(AbstractContextManager):
             **opts
         )
 
-        # HAXOR: bit of a hack, but we don't know how long the env json will be.
-        # TODO: performance optimization: pass the length as the first byte.
-        env_json = ""
-        for block in iter(lambda: os.read(self._fd_read, 1), "\0".encode()):
-            env_json_chunk = block.decode()
-            env_json = env_json + env_json_chunk
+        # message on pipe:
+        # [payload length (plen)][json containing os.environ]
+        #  ^^^^ _BSC bytes ^^^^   ^^^^^^^ plen bytes ^^^^^^^
+        # read json size first:
+        env_json_len = os.read(self._fd_read, self._BSC)
+        env_json_len = int.from_bytes(env_json_len, byteorder="big", signed=False)
+        # read env json
+        env_json = os.read(self._fd_read, env_json_len).decode()
         self.env = json.loads(env_json)
+
+        # Alternative: send f"os.write({self._fd_write}, \"\\0\".encode())"
+        # then read stream bit-by-bit looking for null character. This should be
+        # far less performant than the solution above, but might be more stable:
+        # env_json = ""
+        # for block in iter(lambda: os.read(self._fd_read, 1), "\0".encode()):
+        #     env_json_chunk = block.decode()
+        #     env_json = env_json + env_json_chunk
+        # self.env = json.loads(env_json)
 
         return result
 
