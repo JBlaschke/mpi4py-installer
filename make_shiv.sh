fd14dd970c75e3d607fa32fc7097dd17367b7e34
Johannes Blaschke <johannes@blaschke.science>
2022-12-17 20:34:57 -0800

    clean up pyz version

--- make_shiv.sh
+++ make_shiv.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-ver=0.3.0
-
 pushd $(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
-shiv .                                       \
-    -o shiv/dist/mpi4py_installer-${ver}.pyz \
-    -e mpi4py_installer.cli:run              \
-    --preamble shiv/preamble.py              \
+shiv .                                \
+    -o shiv/dist/mpi4py_installer.pyz \
+    -e mpi4py_installer.cli:run       \
+    --preamble shiv/preamble.py       \
     --compressed
 
 popd
