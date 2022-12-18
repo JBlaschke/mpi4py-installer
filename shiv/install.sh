6f36882d590d7a1b40c24f7a66cf3a5ec2c2ddb8
Johannes Blaschke <johannes@blaschke.science>
2022-12-17 20:46:03 -0800

    improve installer: use tmpdir, use releases

--- shiv/install.sh
+++ shiv/install.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-ver="0.3.0"
 src="JBlaschke/mpi4py-installer"
 name="mpi4py_installer"
+tmpdir=$(mktemp -d)
 
 # Arrange for the temporary file to be deleted when the script terminates
-trap 'rm -f "/tmp/exec.$$"' 0
+trap 'rm -r "${tmpdir}"' 0
 trap 'exit $?' 1 2 3 15
 
 # Create temporary file from curl
-curl -fsSL https://github.com/${src}/raw/main/shiv/dist/${name}-${ver}.pyz >/tmp/exec.$$
+curl -fsSL                                                         \
+    https://github.com/${src}/releases/latest/download/${name}.pyz \
+    > ${tmpdir}/exec.$$
 
 # Make the temporary file executable
-chmod +x /tmp/exec.$$
+chmod +x ${tmpdir}/exec.$$
 
 # Execute the temporary file
-/tmp/exec.$$ $@
+${tmpdir}/exec.$$ $@
