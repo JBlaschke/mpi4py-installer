7397d5053dd46211651ebe65d9fff8df2302e6db
Johannes Blaschke <jpblaschke@lbl.gov>
2024-10-01 16:38:06 -0700

    invariably starting to develop a sophisticated dataclass to hold my settings

--- mpi4py_installer/sites/__init__.py
+++ mpi4py_installer/sites/__init__.py
@@ -3,7 +3,7 @@ import json
 from ..          import load_site, load_user_site, logger, Singleton
 from os          import environ
 from pathlib     import Path
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
@@ -19,6 +19,7 @@ class Site(metaclass=Singleton):
     singleton: the paths are resolved once, and then retrieved from global
     memory on subsequent calls to the constructor.
     """
+
     path: Path = Path(__file__).parent.resolve()
     user_path: Path = Path(
         environ.get("MPI4PY_INSTALLER_SITE_CONFIG", default="~/.mpi4py")
@@ -34,6 +35,7 @@ def find_available_sites() -> tuple[list[str], list[str], Path]:
     current directory, and the directory at `MPI4PY_INSTALLER_SITE_CONFIG`,
     then by enumerating all non-dunder .py files.
     """
+
     logger.debug("Searching for site definitions ...")
     site = Site()
 
@@ -70,6 +72,7 @@ def auto_site() -> str|None:
     * If one site's `check_site` returns `True` return that site's name.
     * If more than one site's `check_site` returns `true` then return `None`
     """
+
     logger.debug("Searching for compatible sites")
 
     available_sites, user_sites, user_sites_root = find_available_sites()
@@ -97,12 +100,66 @@ def auto_site() -> str|None:
     return found
 
 
-CONFIG_DICT = dict[str, dict[str, str]]
+CONFIG_DICT = dict[str, str|list[str]|dict[str, str]]
+
+@dataclass
+class ConfigStore(metaclass=Singleton):
+    file: Path
+    data: CONFIG_DICT|None = field(init=False)
+
+
+    def __post_init__(self):
+        """
+        __post_init__(self)
+
+
+        Load any data contained in the json file with the same file name as
+        `self.file`. E.g. `/path/to/module.py` will attempt to load
+        `/path/to/module.json`. If the json file could not be loaded,
+        `self.valid` is set to False, and `self.data` is set to None
+        """
+
+        module_path = self.file.resolve()
+        config_file = module_path.parent / Path(module_path.stem + ".json")
+        self.data = load_config_file(config_file)
+
+    @property
+    def valid(self) -> bool:
+        """
+        valid -> bool
+
+
+        Data set is valid for processing
+        """
+        return self.data is not None
+
+
+    @property
+    def systems(self) -> list[str]|None:
+        """
+        systems -> list[str]|None
+
+
+        List of all systems contained in this config
+        """
+        assert self.data is not None
+        return site_systems(self.data)
+
+
+    def variants(self, system: str) -> list[str]:
+        """
+        def variants(self, system: str) -> list[str]:
+
+
+        List of all variants on this site for a given system
+        """
+        assert self.data is not None
+        return system_variants(self.data, system)
 
 
 def load_config_file(config_file_path: Path) -> CONFIG_DICT|None:
     """
-    CONFIG_DICT = dict[str, dict[str, str]]
+    CONFIG_DICT = dict[str, str|list[str]|dict[str, str]]
     load_config_file(config_file_path: Path) -> CONFIG_DICT|None
 
 
@@ -113,6 +170,7 @@ def load_config_file(config_file_path: Path) -> CONFIG_DICT|None:
     * If the config file does not exist, or if it's not a json file, then
       return None
     """
+
     if not config_file_path.is_file():
         logger.critical(f"File {config_file_path=} does not exist")
         return None
@@ -131,22 +189,25 @@ def load_config_file(config_file_path: Path) -> CONFIG_DICT|None:
 
 def site_systems(site_config: CONFIG_DICT) -> list[str]:
     """
-    CONFIG_DICT = dict[str, dict[str, str]]
+    CONFIG_DICT = dict[str, str|list[str]|dict[str, str]]
     site_systems(site_config: CONFIG_DICT) -> list[str]
 
 
     Return a list of available sites descirbed in the config dictionary
     """
+
     return [k for k in site_config.keys()]
 
 
 def system_variants(site_config: CONFIG_DICT, system: str) -> list[str]:
     """
-    CONFIG_DICT = dict[str, dict[str, str]]
+    CONFIG_DICT = dict[str, str|list[str]|dict[str, str]]
     system_variants(site_config: CONFIG_DICT, system: str) -> list[str]
 
 
     Return a list of valiable variants descirbed in the config dictionary for
     the specified system
     """
-    return [k for k in site_config[system].keys()]
+    system_config = site_config[system]
+    assert isinstance(system_config, dict)
+    return [k for k in system_config.keys()]
